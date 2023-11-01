1b5096ad22eaa8765ed919070c95f629987b3cd1
Razwa Fazila Wibowo <r.razwafazilaw@gmail.com>
2023-11-01 16:57:12 +0700

    create ThreadRepository interface

---
+++ src/Domains/threads/ThreadRepository.js
@@ -0,0 +1,9 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-unused-vars */
+class ThreadRepository {
+  async addThread(addThread) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = ThreadRepository;
