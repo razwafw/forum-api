9b86d6596cf6455c7a2049b6e66d6e9a22634098
Razwa Fazila Wibowo <r.razwafazilaw@gmail.com>
2023-11-01 16:49:44 +0700

    create AddedThread entity

---
+++ src/Domains/threads/entities/_test/AddedThread.test.js
@@ -0,0 +1,43 @@
+const AddedThread = require('../AddedThread');
+
+describe('an AddedThread entity', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {
+      id: 'userid',
+      title: 'abc',
+    };
+
+    // Action and Assert
+    expect(() => new AddedThread(payload)).toThrowError('ADDED_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      id: true,
+      title: 123,
+      owner: 'owner',
+    };
+
+    // Action and Assert
+    expect(() => new AddedThread(payload)).toThrowError('ADDED_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create addedThread object correctly', () => {
+    // Arrange
+    const payload = {
+      id: 'threadid',
+      title: 'a thread title',
+      owner: 'owner',
+    };
+
+    // Action
+    const { id, title, owner } = new AddedThread(payload);
+
+    // Assert
+    expect(id).toEqual(payload.id);
+    expect(title).toEqual(payload.title);
+    expect(owner).toEqual(payload.owner);
+  });
+});
