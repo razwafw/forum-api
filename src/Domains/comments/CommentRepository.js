2a874ec9fdc57f7ea104c889da5fd56eb9c197a3
Razwa Fazila Wibowo <r.razwafazilaw@gmail.com>
2023-11-05 16:36:01 +0700

    create CommentRepository interface

---
+++ src/Domains/comments/CommentRepository.js
@@ -0,0 +1,9 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-unused-vars */
+class CommentRepository {
+  async addComment(addComment) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = CommentRepository;
