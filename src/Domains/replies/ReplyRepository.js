81c5320035dc70290bfb35f60ae3181e2c4cabf4
Razwa Fazila Wibowo <r.razwafazilaw@gmail.com>
2023-11-08 16:44:18 +0700

    create ReplyRepository interface

---
+++ src/Domains/replies/ReplyRepository.js
@@ -0,0 +1,9 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-unused-vars */
+class ReplyRepository {
+  async addReply(addReply, threadId, commentId, userId) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = ReplyRepository;
