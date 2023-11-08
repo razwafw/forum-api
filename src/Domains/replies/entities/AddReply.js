c3437ef31b97881813dd5ee2e980ccd43e1c7297
Razwa Fazila Wibowo <r.razwafazilaw@gmail.com>
2023-11-08 15:32:27 +0700

    create AddReply entity

---
+++ src/Domains/replies/entities/AddReply.js
@@ -0,0 +1,22 @@
+class AddReply {
+  constructor(payload) {
+    this._verifyPayload(payload);
+
+    const { content } = payload;
+
+    this.content = content;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _verifyPayload({ content }) {
+    if (!content) {
+      throw new Error('ADD_REPLY.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof content !== 'string') {
+      throw new Error('ADD_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+}
+
+module.exports = AddReply;
---
+++ src/Domains/replies/entities/_test/AddReply.test.js
@@ -0,0 +1,34 @@
+const AddReply = require('../AddReply');
+
+describe('an AddReply entity', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {};
+
+    // Action and Assert
+    expect(() => new AddReply(payload)).toThrowError('ADD_REPLY.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      content: 123,
+    };
+
+    // Action and Assert
+    expect(() => new AddReply(payload)).toThrowError('ADD_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create AddReply object correctly', () => {
+    // Arrange
+    const payload = {
+      content: 'a comment reply',
+    };
+
+    // Action
+    const { content } = new AddReply(payload);
+
+    // Assert
+    expect(content).toEqual('a comment reply');
+  });
+});
