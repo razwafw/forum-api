4a675661c4c7510ba988ea501e6b6abd2008dbef
Razwa Fazila Wibowo <r.razwafazilaw@gmail.com>
2023-11-11 10:00:20 +0700

    add getThread function to ThreadRepository

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -4,6 +4,10 @@ class ThreadRepository {
   async addThread(addThread) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async getThread(threadId) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = ThreadRepository;
