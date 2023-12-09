fcb0f31c795b233cbf71b652fa2609c6b1689bfb
Razwa Fazila Wibowo <r.razwafazilaw@gmail.com>
2023-12-09 18:48:35 +0700

    create migrations to create comment_likes table

---
+++ migrations/1702122213096_table-comment-likes.js
@@ -0,0 +1,24 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('comment_likes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    comment_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('comment_likes', 'fk-comment_likes.comment_id-thread_comments.id', 'FOREIGN KEY(comment_id) REFERENCES thread_comments(id) ON DELETE CASCADE');
+  pgm.addConstraint('comment_likes', 'fk-comment_replies.user_id-users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('comment_likes');
+};
